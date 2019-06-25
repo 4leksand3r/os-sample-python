43b0bd44ac6e28010b9dbfe70e12ef9a12bb30e2
4leksand3r <a.aleksanyans@gmail.com>
2019-06-25 18:01:37 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,13 @@ application = Flask(__name__)
 
 @application.route("/producer")
 def producer():
+    
+    prod = KafkaProducer(bootstrap_servers=['my-cluster-kafka-bootstrap:9091'], value_serializer=lambda x: dumps(x).encode('utf-8'))
+    for e in range(1000):
+        data = {'number' : e}
+        prod.send('numtest', value=data)
+    
+    
     return "Kafka producer test"
 
 @application.route("/consumer")
